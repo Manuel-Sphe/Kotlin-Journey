6e00dc8fa305fbe1e8804d82e26a0fce919b7317
Manuel-Sphe <sphesihlemanuel99@gmail.com>
2024-11-09 20:37:14 +0200

    Init commit

---
+++ app/src/test/kotlin/com/org/crud/CruApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.org.crud
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CruApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
